81ee6b721958d3d1bbdff140e088e9b1fc7d3197
antoniomsah <amsah@usp.br>
2024-07-16 21:45:50 -0300

    Add perfect matching tests

---
+++ code/tests/perfect-matching/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "../../algorithms/solver.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  MatchingSolver::initialize();
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> graph(n, vector<int>(n));
+  vector<pair<int, int>> edges(m);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    edges[i] = {u, v};
+    graph[u][v] = graph[v][u] = 1;
+  }
+
+  assert(argc >= 2);
+
+  MatchingSolver solver;
+  solver.set_strategy(stoi(argv[1]));
+  solver.solve(graph, edges);
+
+  return 0;
+}
