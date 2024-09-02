067b82cf27626e22096bc9e536a9b3a9cb2936ec
antoniomsah <amsah@usp.br>
2024-09-02 16:16:06 -0300

    Add test, fixed seed and makefile dependencies

--- code/config.hpp
+++ code/config.hpp
@@ -4,6 +4,7 @@
 extern const int MAX_IT = 1;
 extern const int MOD = 998244353;
 extern const bool TEST = true;
+extern const int SEED = 1281;
 
 extern const int NUM_STRATEGIES = 3;
 
--- code/matrix/tutte-matrix.hpp
+++ code/matrix/tutte-matrix.hpp
@@ -7,17 +7,19 @@
 #include "modular-integer.hpp"
 
 std::mt19937 rng(
-    (int)std::chrono::steady_clock::now().time_since_epoch().count());
+    SEED ? SEED
+         : (int)std::chrono::steady_clock::now().time_since_epoch().count());
 
 template <const int P>
 using TutteMatrix = Matrix<modular_int<P>>;
 
 template <const int T>
-TutteMatrix<T> build_tutte_matrix(const size_t& n, const std::vector<pair<int, int>>& edges) {
+TutteMatrix<T> build_tutte_matrix(const size_t& n,
+                                  const std::vector<pair<int, int>>& edges) {
   TutteMatrix<T> M(n, n);
   for (size_t i = 0; i < edges.size(); i++) {
     modular_int<T> val = rng();
-    const auto &[u, v] = edges[i];
+    const auto& [u, v] = edges[i];
     M(u, v) = val;
     M(v, u) = val * (-1);
   }
